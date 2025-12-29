e9663f72658e301daa2e414c25b8760bfdaa2e7b
anup2134 <anup.s.bhoos@gmail.com>
2025-12-29 23:29:04 +0530

    use awk instead of jq

--- cloudflared_hook/entrypoint.sh
+++ cloudflared_hook/entrypoint.sh
@@ -5,7 +5,7 @@ if ! cloudflared tunnel info openserver-tunnel >/dev/null 2>&1; then
   echo "Tunnel does not exist. Creating..."
   cloudflared tunnel create openserver-tunnel
 
-  tunnel_id=$(cloudflared tunnel list --output json | jq -r '.[] | select(.name=="openserver-tunnel") | .id')
+  tunnel_id=$(cloudflared tunnel list | awk '$2=="openserver-tunnel"{print $1}')
   if [ -z "$tunnel_id" ]; then
     echo "Error: tunnel 'openserver-tunnel' not found" >&2
     exit 1
